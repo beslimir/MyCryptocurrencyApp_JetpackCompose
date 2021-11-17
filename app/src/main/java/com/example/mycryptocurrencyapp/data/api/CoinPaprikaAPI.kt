c84109efb511301ba687eecedd6fa65f3b1fd681
beslimir <marin.beslo@gmail.com>
2021-11-17 16:30:03 +0100

    Move handling from use case to repository

--- app/src/main/java/com/example/mycryptocurrencyapp/data/api/CoinPaprikaAPI.kt
+++ app/src/main/java/com/example/mycryptocurrencyapp/data/api/CoinPaprikaAPI.kt
@@ -2,13 +2,14 @@ package com.example.mycryptocurrencyapp.data.api
 
 import com.example.mycryptocurrencyapp.data.dto.CoinDTO
 import com.example.mycryptocurrencyapp.data.dto.CoinDetailsDTO
+import com.example.mycryptocurrencyapp.data.model.Coin
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CoinPaprikaAPI {
 
     @GET("/v1/coins")
-    suspend fun getAllCoins(): List<CoinDTO>
+    suspend fun getAllCoins(): List<Coin>
 
     @GET("/v1/coins/{coinId}")
     suspend fun getCoinById(@Path("coinId") coinId: String): CoinDetailsDTO
--- app/src/main/java/com/example/mycryptocurrencyapp/domain/use_case/GetAllCoinsUseCase.kt
+++ app/src/main/java/com/example/mycryptocurrencyapp/domain/use_case/GetAllCoinsUseCase.kt
@@ -14,16 +14,8 @@ class GetAllCoinsUseCase @Inject constructor(
     private val coinRepository: CoinRepository
 ) {
 
-    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
-        try {
-            emit(Resource.Loading<List<Coin>>())
-            val coins = coinRepository.getAllCoins().map { it.toCoin() }
-            emit(Resource.Success<List<Coin>>(coins))
-        } catch(e: HttpException) {
-            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occurred"))
-        } catch(e: IOException) {
-            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet connection."))
-        }
+    operator fun invoke(): Flow<Resource<List<Coin>>> {
+        return coinRepository.getAllCoins()
     }
 
 }
